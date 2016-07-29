8b8c0347d116f6cba1cc4f8388e1b5c936374583
Daniel Kuehn <daniel@kuehn.se>
2016-07-29 12:05:45 +0000

    Added the initgroup function forward declaration and function member in the mod_group struct

--- include/modules.h
+++ include/modules.h
@@ -11,10 +11,12 @@ typedef enum nss_status (*nss_getgrnam_r)(const char*, struct group*, char*, siz
 typedef enum nss_status (*nss_getgrgid_r)(gid_t, struct group*, char*, size_t, int*);
 typedef enum nss_status (*nss_getpwnam_r)(const char*, struct passwd*, char*, size_t, int*);
 typedef enum nss_status (*nss_getpwuid_r)(uid_t, struct passwd*, char*, size_t, int*);
+typedef enum nss_status (*initgroups_dyn_function)(const char*, gid_t, long int*, long int*, gid_t**, long int, int*);
 
 struct mod_group {
 	nss_getgrnam_r nss_getgrnam_r;
 	nss_getgrgid_r nss_getgrgid_r;
+	initgroups_dyn_function initgroups_dyn_function;
 	action on_status[4];
 	link_t link;
 };
